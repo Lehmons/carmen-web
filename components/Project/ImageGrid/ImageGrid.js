237d63ebc4d79724494559ed76e9ed5739a02f8c
Lehmons <freddiejackson217@gmail.com>
2023-03-08 11:59:28 +0000

    Removed clickEvent on grid

--- components/Project/ImageGrid/ImageGrid.js
+++ components/Project/ImageGrid/ImageGrid.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import ImageGridStyles from "./ImageGrid.styled";
 import ImageBlock from "../../ImageBlock";
 
-export default function ImageGrid({ images, onClick, counter }) {
+export default function ImageGrid({ images }) {
   return (
     <ImageGridStyles>
       {images?.map((item, i) => (
-				<section key={i} onClick={e => onClick(e, i + counter)}>
-					<ImageBlock
-						title={item?.image?.alt}
-						text={item?.image?.alt}
-						image={item?.image}
-						asset={item?.image?.asset}
-						isThumb={false}
-						hasPaddingBottom={true}
-						key={i}
-					/>
-				</section>
+        <section key={i}>
+          <ImageBlock
+            title={item?.image?.alt}
+            text={item?.image?.alt}
+            image={item?.image}
+            asset={item?.image?.asset}
+            isThumb={false}
+            hasPaddingBottom={true}
+            key={i}
+          />
+        </section>
       ))}
     </ImageGridStyles>
   );
